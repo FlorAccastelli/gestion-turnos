9ac6ce4735d0e593e84430a9f92a0b7c01100cda
Florencia Accastelli <f.accastelli@gmail.com>
2024-06-14 18:09:26 -0300

    configured environment

---
+++ server/dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+const num1 = 15;
+const num2 = 20;
+const sumar = (a, b) => a + b;
+console.log(sumar(num1, num2));
+const nombre = "Florencia";
---
+++ server/index.ts
@@ -0,0 +1,8 @@
+const num1: number = 15
+const num2: number = 20
+
+const sumar = (a: number, b: number) : number => a + b;
+
+console.log(sumar(num1, num2));
+
+const nombre: string = "Florencia";
